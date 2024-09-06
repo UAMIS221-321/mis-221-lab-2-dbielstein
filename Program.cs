d15e113a21203996b65bc47a9eea7065539c2f8a
dbielstein <dbielstein@crimson.ua.edu>
2024-09-06 13:54:25 -0500

    Some message

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("What is your name? ");
+
+string name = Console.ReadLine();
+
+Console.WriteLine(name);
